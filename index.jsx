1779fb36123e6cc40c2e67176b3521622d792d47
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2021-11-04 14:51:33 -0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FBpost from "./postFB"
+
+
+
+const rootElement = document.getElementById("root");
+
+
+
+
+
+ const element = <FBPost/>
+
+ ReactDOM.render(element, rootElement);
